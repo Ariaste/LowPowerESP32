7d8595090ee6ef8f441d7b837869fad4e7cf3acb
Patrick Fock <fock@students.uni-marburg.de>
2022-05-21 19:47:52 +0200

    created lib ClimateSensors

--- src/ClimateSensors.h
+++ src/ClimateSensors.h
@@ -1,7 +1,7 @@
 #include <Adafruit_HTU21DF.h>
 #include <Adafruit_BMP085.h>
 
-/** This class unites the HTD21DF and the BMP180 sensor. */
+/** This class unites the HTD21DF and the BMP085 sensor. */
 class ClimateSensor {
 
     private:
@@ -28,7 +28,7 @@ class ClimateSensor {
         }
 
         /**
-         * Approximates the real temperature by giving an average of the HTU21DF and BMP180 temperature readings.
+         * Approximates the real temperature by giving an average of the HTU21DF and BMP085 temperature readings.
          * @return float temperature
          */
         float readTemperature() {
@@ -36,31 +36,50 @@ class ClimateSensor {
         }
 
         /**
-         * 
+         * Reads the temperature using the HTU21DF sensor.
+         * @return float temperature
          */
         float readTemperatureHTU21DF() {
             return humiditySensor.readTemperature();
         }
 
         /**
-         * 
+         * Reads the temperature using the BMP180 sensor.
+         * @return float temperature
          */
-        float readTemperatureBMP180() {
+        float readTemperatureBMP085() {
             return barometricSensor.readTemperature();
         }
 
         /**
-         * 
+         * Reads the pressure using the BMP180 sensor.
+         * @return float pressure
          */
         float readPressure() {
             return barometricSensor.readPressure();
         }
 
         /**
-         * 
+         * Calculates the pressure at sealevel.
+         * @return float altitude at sealevel
          */
-        float readAltitude(float sealevelPressure=101325) {
+        float readSeaLevelPressure(float altitude_meters = 0) {
+            return barometricSensor.readSealevelPressure(altitude_meters);
+        }
+
+        /**
+         * Reads the altitude according to an given sealevel pressure. The default is 1013.25 mP.
+         * @return float altitude
+         */
+        float readAltitude(float sealevelPressure = 101325) {
             return barometricSensor.readAltitude(sealevelPressure);
         }
+
+        /**
+         * Resets the HTU21DF with a 15 ms delay.
+         */
+         void resetHTU21DF() {
+             humiditySensor.reset();
+         }
         
 };
