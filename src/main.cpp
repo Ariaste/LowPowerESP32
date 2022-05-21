fcfd19fb4fd080da84456f294b6d8b43cd79d768
Patrick Fock <fock@students.uni-marburg.de>
2022-05-21 19:32:30 +0200

    hide

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,14 @@
 #include <Arduino.h>
-#include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <ClimateSensors.h>
 
-BluetoothSerial SerialBT;
+const char* ssid = "Mau";
+const char* password = "nzikatze";
+const int led = 26;
+
 
 void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("Maunzikatze"); //Name des ESP32
-  Serial.println("Der ESP32 ist bereit. Verbinde dich nun über Bluetooth.");
-  BTScanResults* devices = SerialBT.discover();
-
+  
 }
 
-void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(25);
-}
+void loop() { }
